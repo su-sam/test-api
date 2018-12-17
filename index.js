4f9fc6a26dfdce8bf65425c1cceb9a1538c29df9
intern <nana15492@gmail.com>
2018-12-17 09:29:07 +0700

    updated

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const home = require('./routes/home');
 const db = require('./routes/db');
 const user = require('./routes/user');
 const express = require('express');
+
 const app = express();
 
 //router that url path
 app.use('/', home);
 app.use('/db',db);
-app.use('/db/user',user);
+app.use('/user',user);
 
-
+//network connection
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- routes/user.js
+++ routes/user.js
@@ -1,27 +1,50 @@
 const express = require('express');
+const Joi = require('joi');
+const userDb = require('../modules/user');
 const router = express.Router();
 
+//test JSON
 const user = [
-    {vehicle_no : 'abc', id : 1, name : 'Suchada', last_online : Date, online_status : 0},
-    {vehicle_no : 'we3', id : 2, name : 'Manusawee', last_online : Date, online_status : 0},
-    {vehicle_no : 'r4e', id : 3, name : 'Kemruji', last_online : Date, online_status : 0}
+    {vehicle_no : 'abc', id : 1, name : 'Suchada', last_online : JSON.stringify(new Date), online_status : 0},
+    {vehicle_no : 'we3', id : 2, name : 'Manusawee', last_online : JSON.stringify(new Date), online_status : 0},
+    {vehicle_no : 'r4e', id : 3, name : 'Kemruji', last_online : JSON.stringify(new Date), online_status : 0}
 ]
 
 router.get('/',(req,res)=>{
     res.send(user);
 });
 
+//find by vehicle no
 router.get('/:vehicle_no',(req,res)=>{
     const v_obj = user.find(u => u.vehicle_no === req.params.vehicle_no);
     if(!v_obj) res.status(404).send('User not found');
     else res.send(toggleStatus(v_obj));
 });
 
+//post for add new member
+
+//put for update a member
+router.put('/:vehicle_no',(req,res)=>{
+    const v_obj = user.find(u => u.vehicle_no === req.params.vehicle_no);
+    if(!v_obj) res.status(404).send('User not found');
+
+    
+});
+//delete for delete a member
+
+//toggle user_status for online-offline and update DATETIME
 function toggleStatus(v_obj){
+
     let user_status = v_obj.online_status;
-    user_status = !(user_status);
+
+    user_status = !(user_status); //toggle status
     v_obj.online_status = user_status;
+
+    //update time only true->false for last online time
+    if(!user_status) v_obj.last_online = JSON.stringify(new Date());
+
     return v_obj
 }
+
 
 module.exports = router;
